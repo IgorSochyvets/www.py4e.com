3da81177efd91baece4910b2e6d9981af9351f74
Ihor Sochyvets <Ihor.Sochyvets@ibm.com>
2021-02-14 14:06:05 +0200

    4.7.py - return fix

--- 4_Functions/4.7.py
+++ 4_Functions/4.7.py
@@ -2,20 +2,20 @@
 # that takes a score as its parameter and returns a grade as a string.
 
 def computegrade(score):
-    if score >= 10.0:
+    if score >= 10.00:
         print("Bad score")
         exit()
-    elif score >= 9.0:
+    elif score >= 9.00:
         grade = "A"
-    elif score >= 8.0:
+    elif score >= 8.00:
         grade = "B"
-    elif score >= 7.0:
+    elif score >= 7.00:
         grade = "C"
-    elif score >= 6.0:
+    elif score >= 6.00:
         grade = "D"    
     else:
         grade = "F"        
-    return(grade)
+    return grade
 
 scoreInput = input("Enter score: ")
 
