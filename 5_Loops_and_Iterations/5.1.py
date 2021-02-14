f2756659a59a1413801a3f039a2161d3feeb7e79
Ihor Sochyvets <Ihor.Sochyvets@ibm.com>
2021-02-14 15:59:43 +0200

    5.1.py added

---
+++ 5_Loops_and_Iterations/5.1.py
@@ -0,0 +1,22 @@
+# Exercise 1: Write a program which repeatedly reads numbers until the user enters “done”. 
+# Once “done” is entered, print out the total, count, and average of the numbers. If the 
+# user enters anything other than a number, detect their mistake using try and except and 
+# print an error message and skip to the next number.
+
+total = 0
+count = 0
+average = 0
+while True:
+    try:
+        line = input('> ')
+        if line == 'done':
+            break
+        lineInt = int(line)
+        count = count + 1
+        print("Count: ",count)
+        total = total + lineInt
+        print("Total: ", total) 
+    except:
+        print("Error, put a number")
+    print(lineInt)
+print("Count: ", count, "Total: ", total, "Average: ", total/count)
